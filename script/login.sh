dc4ae3af1d1dad27c2292c834c83f29dfcc58f7a
Chen Xiaojie <caliburn1994@gmail.com>
2021-03-30 21:27:21 +0900

    login ttl

---
+++ script/login.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")"/.. >/dev/null 2>&1 && pwd)"
+CURRENT_DIR=$(dirname "$0")
+BASENAME=$(basename "$0" | cut -f 1 -d '.')
+TEMPLATE="${ROOT_DIR}/template/${BASENAME}.ttl"
+TTL="${ROOT_DIR}/ttl/${BASENAME}.ttl"
+
+
+# data
+export my_ip="192.168.0.111"
+export my_username="xiaojie"
+export my_password="mypassword"
+
+# output
+envsubst <"${TEMPLATE}" >"${TTL}"
