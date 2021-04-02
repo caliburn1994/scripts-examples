853e4e313c17ebc4a21ab36f84b8cfe30c9d3e81
Chen Xiaojie <caliburn1994@gmail.com>
2021-04-02 10:56:25 +0900

    vagrant private key login

--- tera-term/script/vagrant/login.sh
+++ tera-term/script/vagrant/login.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
-ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")"/.. >/dev/null 2>&1 && pwd)"
 CURRENT_DIR=$(dirname "$0")
 BASENAME=$(basename "$0" | cut -f 1 -d '.')
-TEMPLATE="${ROOT_DIR}/template/${BASENAME}.ttl"
-TTL="${ROOT_DIR}/ttl/${BASENAME}.ttl"
+TTL="${CURRENT_DIR}/${BASENAME}.ttl"
 
 
-# output
-envsubst <"${TEMPLATE}" >"${TTL}"
+# tll file
+cat << EOF > "${TTL}"
+connect '127.0.0.1:2222 /ssh /auth=publickey /user=vagrant /keyfile=${CURRENT_DIR}\.vagrant\machines\default\virtualbox\private_key '
+EOF
+
+# run
+ "/c/Program Files (x86)/teraterm/ttpmacro.exe"  "${TTL}"
